e7608677458357b4f28d58998a0f4200436d4bad
Pavel <pavelkulik@hotmail.com>
2020-01-19 19:06:53 -0800

    Add range parser

--- Source/RecordChannelSelector.cpp
+++ Source/RecordChannelSelector.cpp
@@ -1,8 +1,11 @@
 #include "RecordChannelSelector.h"
+#include <string>
+#include <vector>
 
 ChannelButton::ChannelButton(int _id) : Button(String(_id)), id(_id) {
     setClickingTogglesState(true);
 }
+
 ChannelButton::~ChannelButton() {}
 
 void ChannelButton::paintButton(Graphics &g, bool isMouseOver, bool isButtonDown)
@@ -74,15 +77,8 @@ RangeEditor::~RangeEditor() {}
  * RECORD CHANNEL SELECTOR
 ***************************/
 
-RecordChannelSelector::RecordChannelSelector(int nChannels) : Component()
+RecordChannelSelector::RecordChannelSelector(int nChannels) : Component(), nChannels(nChannels)
 {
-    //Creates the grid based on the number of channel
-
-    //Max width = 20*24;
-
-    //If <= 64, generate one rows of 8
-    //If <= 256, generate rows of 16
-    //If <= 512, generate rows of 24
 
     int nColumns = 16;
     int nRows = nChannels / nColumns + (int)(!(nChannels % nColumns == 0));
@@ -98,7 +94,7 @@ RecordChannelSelector::RecordChannelSelector(int nChannels) : Component()
             {
                 channelButtons.add(new ChannelButton(nColumns*i+j+1));
                 channelButtons.getLast()->setBounds(width/nColumns*j, height/nRows*i, buttonSize, buttonSize);
-                //channelButtons.getLast()->addListener(this);
+                channelButtons.getLast()->addListener(this);
                 addChildAndSetID(channelButtons.getLast(), String(nColumns*i+j+1));
             }
 			
@@ -108,24 +104,29 @@ RecordChannelSelector::RecordChannelSelector(int nChannels) : Component()
     //Add "SELECT ALL" button
 	selectButtons.add(new SelectButton("ALL"));
     selectButtons.getLast()->setBounds(0, height, 0.25*width, width / nColumns);
+    selectButtons.getLast()->addListener(this);
 	addChildAndSetID(selectButtons.getLast(),"ALL");
 
     //Add "SELECT NONE" button
 	selectButtons.add(new SelectButton("NONE"));
     selectButtons.getLast()->setBounds(0.25*width, height, 0.25*width, width / nColumns);
+    selectButtons.getLast()->addListener(this);
 	addChildAndSetID(selectButtons.getLast(),"ALL");
     
     if (nChannels > 8)
     {
+
         //Add "SELECT RANGE" button
-        selectButtons.add(new SelectButton("RANGE:"));
+        selectButtons.add(new SelectButton("RANGE"));
         selectButtons.getLast()->setBounds(0.5*width, height, 0.25*width, width / nColumns);
+        selectButtons.getLast()->addListener(this);
         addChildAndSetID(selectButtons.getLast(),"ALL");
 
-        //Add Range Edtior
+        //Add Range Editor
         
         rangeEditor = new RangeEditor("Range");
         rangeEditor->setBounds(0.75*width, height, 0.25*width, width / nColumns);
+        rangeEditor->addListener(this);
         addChildAndSetID(rangeEditor,"RANGE_EDITOR");
         
     }
@@ -139,3 +140,311 @@ RecordChannelSelector::RecordChannelSelector(int nChannels) : Component()
 }
 
 RecordChannelSelector::~RecordChannelSelector() {}
+
+void RecordChannelSelector::textEditorReturnKeyPressed(TextEditor& editor)
+{
+    channelStates = parseStringIntoRange(9);
+
+    if (channelStates.size() < 3)
+        return;
+
+    for (int i = 0; i < nChannels; i++)
+        channelButtons[i++]->setToggleState(false, NotificationType::dontSendNotification);
+
+    int i = 0;
+    while (i <= channelStates.size() - 3)
+    {
+        const int lim = channelStates[i+1];
+        const int comd = channelStates[i+2];
+        for (int fa = channelStates[i]; fa < lim; fa += comd)
+        {
+            channelButtons[i++]->setToggleState(true, NotificationType::dontSendNotification);
+        }
+        i+=3;
+    }
+
+    /*
+    parseStringIntoRange returns an array of size N where N/3 is the number of ranges to set.
+    Each range "tuple" is represented by (startIdx, endIndex, step)
+    */
+
+    for (auto val : channelStates)
+    {
+        std::cout << val << ",";
+    }
+    std::cout << std::endl;
+
+    i = 0;
+    while (i < nChannels)
+    {
+        channelButtons[i++]->setToggleState(false, NotificationType::dontSendNotification);
+    }
+
+    for (i = 0; i < channelStates.size(); i += 3)
+    {
+        int startIdx = channelStates[i];
+        int endIdx = channelStates[i+1];
+        int step = channelStates[i+2];
+        
+        int ch = startIdx;
+        while (ch < endIdx)
+        {
+            channelButtons[ch]->setToggleState(true, NotificationType::dontSendNotification);
+            ch+=step;
+        }
+
+    }
+}
+
+void RecordChannelSelector::buttonClicked(Button* button)
+{
+    
+    for (auto* button : selectButtons)
+    {
+        button->setToggleState(false, NotificationType::dontSendNotification);
+    }
+    
+    if (button->getButtonText() == String("ALL"))
+    {
+        std::cout << "Select all channels" << std::endl;
+        int i = 0;
+        while (i < nChannels)
+        {
+            channelButtons[i++]->setToggleState(true, NotificationType::dontSendNotification);
+        }
+        rangeString = "[" + String(1) + ":" + String(nChannels) + "]";
+        
+        button->setToggleState(true, NotificationType::dontSendNotification);
+        
+    }
+    else if (button->getButtonText() == String("NONE"))
+    {
+        std::cout << "Select no channels" << std::endl;
+        int i = 0;
+        while (i < nChannels)
+        {
+            channelButtons[i++]->setToggleState(false, NotificationType::dontSendNotification);
+        }
+        rangeString = "[]";
+        
+        button->setToggleState(true, NotificationType::dontSendNotification);
+    }
+    else if (button->getButtonText() == String("RANGE:"))
+    {
+        std::cout << "Set custom range of channels" << std::endl;
+        button->setToggleState(true, NotificationType::dontSendNotification);
+    }
+    else //channel button was manually selected
+    {
+        selectButtons.getLast()->setToggleState(true, NotificationType::dontSendNotification);
+        int channel = ((ChannelButton*)button)->getId();
+        updateRangeString();
+    }
+    
+    rangeEditor->setText(rangeString);
+    
+}
+
+void RecordChannelSelector::updateRangeString()
+{
+    
+    rangeString = "";
+    
+    int startIdx = 0;
+    int endIdx = 0;
+
+    bool inRange = false;
+    
+    for (int i = 0; i < nChannels; i++)
+    {
+        if (channelButtons[i]->getToggleState())
+        {
+            if (inRange)
+            {
+                if (i == nChannels-1)
+                {
+                    rangeString += (rangeString == "" ? "" : ",");
+                    rangeString += String(startIdx) + ":" + String(nChannels);
+                }
+            }
+            else
+            {
+                if (i == nChannels-1)
+                {
+                    rangeString += (rangeString == "" ? "" : ",");
+                    rangeString += String(nChannels);
+                }
+                startIdx = i + 1;
+            }
+            inRange = true;
+            endIdx = i + 1;
+        }
+        else
+        {
+            if (inRange)
+            {
+                rangeString += (rangeString == "" ? "" : ",");
+                if (startIdx < endIdx)
+                    rangeString += String(startIdx) + ":" + String(endIdx);
+                else
+                    rangeString += String(startIdx);
+            }
+            else
+            {
+                
+            }
+            inRange = false;
+        }
+        
+    }
+    
+}
+
+int RecordChannelSelector::convertStringToInteger(String s)
+{
+    char ar[20];
+    int i, k = 0;
+    for (i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= 48 && s[i] <= 57)
+        {
+            ar[k] = s[i];
+            k++;
+        }
+    }
+    if (k > 7)
+    {
+        return 1000000;
+    }
+    ar[k] = '\0';
+    k = atoi(ar);
+    return k;
+}
+
+Array<int> RecordChannelSelector::parseStringIntoRange(int rangeValue)
+{
+    String s = ",";
+    s += rangeEditor->getText();
+    Array<int> finalList, separator, rangeseparator;
+    int i, j, a, b, k, openb, closeb, otherchar, x, y;
+    s += ",";
+    for (i = 0; i < s.length(); i++) //split string by ' , ' or ' ; '
+    {
+        if (s[i] == ';' || s[i] == ',')
+        {
+            separator.add(i);
+        }
+    }
+    for (i = 0; i < separator.size() - 1; i++) // split ranges by ' : ' or ' - '
+    {
+        j = k = separator[i] + 1;
+        openb = closeb = otherchar = 0;
+        rangeseparator.clear();
+        for (; j < separator[i + 1]; j++)
+        {
+            if (s[j] == '-' || s[j] == ':')
+            {
+                rangeseparator.add(j);
+            }
+            else if (((int)s[j] == 32))
+            {
+                continue;
+            }
+            else if (s[j] == '[' || s[j] == '{' || s[j] == '(')
+            {
+                openb++;
+            }
+            else if (s[j] == ']' || s[j] == '}' || s[j] == ')')
+            {
+                closeb++;
+            }
+            else if ((int)s[j] > 57 || (int)s[j] < 48)
+            {
+                otherchar++;
+            }
+        }
+
+        if (openb != closeb || openb > 1 || closeb > 1 || otherchar > 0) //Invalid input
+        {
+            continue;
+        }
+
+        for (x = separator[i] + 1; x < separator[i + 1]; x++) //trim whitespace and brackets from front
+        {
+            if (((int)s[x] >= 48 && (int)s[x] <= 57) || s[x] == ':' || s[x] == '-')
+            {
+                break;
+            }
+        }
+        for (y = separator[i + 1] - 1; y > separator[i]; y--) //trim whitespace and brackets from end
+        {
+            if (((int)s[y] >= 48 && (int)s[y] <= 57) || s[y] == ':' || s[y] == '-')
+            {
+                break;
+            }
+        }
+        if (x > y)
+        {
+            continue;
+        }
+
+        if (rangeseparator.size() == 0) //syntax of form - x or [x]
+        {
+            a = convertStringToInteger(s.substring(x, y + 1));
+            if (a == 0 || a > rangeValue)
+            {
+                continue;
+            }
+            finalList.add(a - 1);
+            finalList.add(a - 1);
+            finalList.add(1);
+        }
+        else if (rangeseparator.size() == 1) // syntax of type - x-y or [x-y]
+        {
+            a = convertStringToInteger(s.substring(x, rangeseparator[0]));
+            b = convertStringToInteger(s.substring(rangeseparator[0] + 1, y + 1));
+            if (a == 0)
+            {
+                a = 1;
+            }
+            if (b == 0)
+            {
+                b = rangeValue;
+            }
+            if (a > b || a > rangeValue || b > rangeValue)
+            {
+                continue;
+            }
+            finalList.add(a - 1);
+            finalList.add(b - 1);
+            finalList.add(1);
+        }
+        else if (rangeseparator.size() == 2) // syntax of type [x:y:z] or x-y-z
+        {
+            a = convertStringToInteger(s.substring(x, rangeseparator[0] + 1));
+            k = convertStringToInteger(s.substring(rangeseparator[0] + 1, rangeseparator[1]));
+            b = convertStringToInteger(s.substring(rangeseparator[1] + 1, y + 1));
+
+            if (a == 0)
+            {
+                a = 1;
+            }
+            if (b == 0)
+            {
+                b = rangeValue;
+            }
+            if (k == 0)
+            {
+                k = 1;
+            }
+            if (a > b || a > rangeValue || b > rangeValue)
+            {
+                continue;
+            }
+            finalList.add(a - 1);
+            finalList.add(b - 1);
+            finalList.add(k);
+        }
+    }
+    return finalList;
+}
--- Source/RecordChannelSelector.h
+++ Source/RecordChannelSelector.h
@@ -1,10 +1,13 @@
 #include <EditorHeaders.h>
 
+enum Select { ALL, NONE, RANGE };
+
 class ChannelButton : public Button	
 {
 public:
 	ChannelButton(int id);
 	~ChannelButton();
+    int getId() { return id; };
 private:
 	int id; 
     int width;
@@ -30,13 +33,24 @@ private:
 	//TODO:
 };
 
-class RecordChannelSelector : public Component
+class RecordChannelSelector : public Component, public Button::Listener, public TextEditor::Listener
 {
 public:
 	RecordChannelSelector(int nChannels);
 	~RecordChannelSelector();
 private:
+	int convertStringToInteger(String s);
+	Array<int> parseStringIntoRange(int rangeValue);
+	void buttonClicked(Button *);
+	void textEditorReturnKeyPressed(TextEditor &);
+	void updateRangeString();
+	void parseRangeString();
 	OwnedArray<ChannelButton> channelButtons;
 	OwnedArray<SelectButton> selectButtons;
 	ScopedPointer<RangeEditor> rangeEditor;
+
+	int nChannels;
+	String rangeString;
+	Array<int> channelStates;
+    
 };
